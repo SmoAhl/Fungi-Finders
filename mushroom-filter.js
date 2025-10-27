b6d4b7b25738f11ed3927e9b91cac2759622d0fb
Simo Ahlqvist <p19497@edu.sasky.fi>
2025-10-27 11:34:21 +0200

    Adding animations. page view transition, nav list, card filtering and hero scroll

--- mushroom-filter.js
+++ mushroom-filter.js
@@ -8,13 +8,23 @@ const currentFilters = {
   edible: "all",
 };
 
+cards.forEach((card, index) => {
+  const mushroomId = `mushroom-${index + 1}`;
+  card.style.viewTransitionName = `mushroom-card-${mushroomId}`;
+});
+
 seasonalFilter.addEventListener("change", updateFilter);
 edibleFilter.addEventListener("change", updateFilter);
 
 function updateFilter(e) {
   const filterType = e.target.name;
   currentFilters[filterType] = e.target.value;
-  filterCards();
+
+  if (!document.startViewTransition()) {
+    filterCards();
+    return;
+  }
+  document.startViewTransition(() => filterCards());
 }
 
 function filterCards() {
